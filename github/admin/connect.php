1c8bcd3f4b6259c0aa96679b8c2e0d7d434f54ee
dominika-zak <dominikaa.zak@gmail.com>
2016-03-18 09:33:44 -0400

    Update connect.php

--- github/admin/connect.php
+++ github/admin/connect.php
@@ -1,12 +1,23 @@
-<?
-ob_start();
-try
-{
-    $db = new PDO('mysql:host=localhost;dbname=dominika-zak', 'i1759707_wp1', 'D]C9SX[Xaf40.(5');
- $db->exec("set names utf8");
-}
-catch (PDOException $e)
-{
-    print "Błąd połączenia z bazą!: " . $e->getMessage() . "<br/>";
-    die();
+<?
+
+ob_start();
+
+try
+
+{
+
+    $db = new PDO('mysql:host=localhost;dbname=dominika-zak', 'i1759707_wp1', 'xxxxxxx');
+
+ $db->exec("set names utf8");
+
+}
+
+catch (PDOException $e)
+
+{
+
+    print "Błąd połączenia z bazą!: " . $e->getMessage() . "<br/>";
+
+    die();
+
 } 
